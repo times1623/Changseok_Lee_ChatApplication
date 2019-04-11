13741edcb126265525fef1d7935dec6b22034112
Lee Changseok <leechangseok@Lees-MacBook-Pro.local>
2019-04-11 01:56:05 -0400

    chat app stylize and minor error fix

---
+++ public/js/modules/themeChange.js
@@ -0,0 +1,7 @@
+function changeTheme() {
+    var theme = document.body.classList.add("themeChange");
+}
+
+function disableTheme() {
+    var disable = document.body.classList.remove("themeChange");
+}
